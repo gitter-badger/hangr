b1d043a136fde3cd185e5a44209bdc8404f27c39
David Clutter <cluttered.code@gmail.com>
2015-10-14 23:22:25 -0600

    Add Hapi server and test

---
+++ test/index_test.js
@@ -0,0 +1,17 @@
+var Hangr = require('..');
+var Code = require('code');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('index', function () {
+
+    it('contains a hapi server', function (done) {
+
+        expect(Hangr.hapi).not.to.be.null();
+        done();
+    });
+});
