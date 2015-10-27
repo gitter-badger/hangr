2c9c3bb7aab3565ceba4351b3ddb88a74f69f706
David Clutter <cluttered.code@gmail.com>
2015-10-27 12:08:14 -0600

    improve code climate of hangr.js

--- bin/hangr.js
+++ bin/hangr.js
@@ -5,6 +5,19 @@ var cli = require('commander'),
     fs = require('fs'),
     npm = require('npm');
 
+var addNpmCommand = function (command, description) {
+    cli.command(command)
+        .description(description)
+        .action(function () {
+            npm.load({}, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                npm.commands.run([command]);
+            });
+        });
+};
+
 cli.version('v' + require('../package.json').version);
 
 cli.command('create [appName]')
@@ -13,32 +26,12 @@ cli.command('create [appName]')
         create.interactiveCreate(appName);
     });
 
-cli.command('start')
-    .description('run application in "development" mode')
-    .action(function (env) {
-        npm.load({}, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            npm.commands.run(['start']);
-        });
-    });
+addNpmCommand('start', 'run application in "development" mode');
+addNpmCommand('deploy', 'run application in "production" mode');
 
-cli.command('deploy')
-    .description('run application in "production" mode')
-    .action(function (env) {
-        npm.load({}, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            npm.commands.run(['deploy']);
-        });
-    });
-
-
-if (process.argv.length > 2)
+if (process.argv.length > 2) {
     cli.parse(process.argv);
-else {
+} else {
     fs.access(process.cwd() + '/server.js', fs.F_OK, function (err) {
         if (err) {
             console.error('No hangr project exists. Try running "hangr create <appName>"');
